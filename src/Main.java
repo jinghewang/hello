ea3bf2af663217af1db9d92481130f7a46bb11e6
jinghewang <jinghewang@163.com>
2016-02-02 16:57:26 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Integer i = 2;
+        System.out.println("i:"+i.toString());
+        i++;
+        System.out.println("i:"+i.toString());
+        System.out.println("Hello World!");
+    }
+}
